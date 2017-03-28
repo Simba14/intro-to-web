6684b4dd0fd0618fe06a125f490bd18d2f75ca71
Simba14 <seanblundell@gmail.com>
2017-03-28 13:54:03 +0100

    Added form method to get cat name

--- app.rb
+++ app.rb
@@ -13,7 +13,19 @@ get '/roi' do
   "He's watching"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb :index
 end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb :index
+end
+
+# post '/named-cat' do
+#   p params
+#   @name = params[:name]
+#   erb :index
+# end
