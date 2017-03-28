c33d5a7a5e7e9b031fa9a214c23398e481ceca6f
Simba14 <seanblundell@gmail.com>
2017-03-28 11:14:48 +0100

    Added cat path /cat with kitten image

--- app.rb
+++ app.rb
@@ -1,13 +1,20 @@
 require 'sinatra'
+set :session_secret, 'super secret'
 
 get '/' do
   "hello!"
 end
 
-get '/secret/' do
+get '/secret' do
   "Harry Potter is Theresa May in disguise."
 end
 
-get '/roi/' do
+get '/roi' do
   "He's watching"
+end
+
+get '/cat' do
+  "<div style='border: dashed red 3px'>
+  <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'>
+  </div>"
 end
